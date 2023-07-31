7b1ccfbf0715d718f37635b127c1a9bfbc08a058
MrAke <aketostoboy@gmail.com>
2023-07-31 15:10:54 +0500

    Add project files.

---
+++ LeetCode-Solutions/1-TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (numDict.ContainsKey(complement))
+            {
+                return new int[] { numDict[complement], i };
+            }
+            numDict[nums[i]] = i;
+        }
+
+        return new int[0];
+    }
+}
