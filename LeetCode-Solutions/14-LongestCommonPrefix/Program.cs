1ed808a35a64d77a3eeec95647e4135672b59721
MrAke <aketostoboy@gmail.com>
2023-07-31 15:18:44 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/14-LongestCommonPrefix/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = "";
+
+        if (strs == null || strs.Length == 0)
+        {
+            return prefix;
+        }
+
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            char currentChar = strs[0][i];
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i == strs[j].Length || strs[j][i] != currentChar)
+                {
+                    return prefix;
+                }
+            }
+
+            prefix += currentChar;
+        }
+
+        return prefix;
+    }
+}
