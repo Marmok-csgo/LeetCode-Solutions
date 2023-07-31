e4c65fca6713d8d95590dd1066e51ac3a5701544
MrAke <aketostoboy@gmail.com>
2023-07-31 15:33:50 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/27-RemoveElement/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int current = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[current] = nums[i];
+                current++;
+            }
+        }
+        return current;
+    }
+}
