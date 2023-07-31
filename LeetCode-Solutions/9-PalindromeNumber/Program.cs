211a0618afdf7cc609832ccf9c35bc5ff5ef8971
MrAke <aketostoboy@gmail.com>
2023-07-31 15:12:59 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/9-PalindromeNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+        var reverse = 0;
+        var temp = x;
+        while (temp > 0)
+        {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return x == reverse;
+    }
+}
