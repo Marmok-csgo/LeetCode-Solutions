8015133b00d0cf3ff4c16bbe10ded42d045dec85
MrAke <aketostoboy@gmail.com>
2023-07-31 15:21:07 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/20-ValidParentheses/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char ch in s)
+        {
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                stack.Push(ch);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                if ((ch == ')' && stack.Peek() == '(') ||
+                    (ch == '}' && stack.Peek() == '{') ||
+                    (ch == ']' && stack.Peek() == '['))
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
