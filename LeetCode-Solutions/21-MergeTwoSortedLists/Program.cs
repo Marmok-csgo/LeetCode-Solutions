b9beb79142baaac490aba61659211ad3c4cda73b
MrAke <aketostoboy@gmail.com>
2023-07-31 15:24:28 +0500

    LeetCodeSolution

---
+++ LeetCode-Solutions/21-MergeTwoSortedLists/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = list1 ?? list2;
+
+        return dummy.next;
+    }
+}
