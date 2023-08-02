ad110960a11378781db244c4b67dda5094582bb6
MrAke <aketostoboy@gmail.com>
2023-08-02 16:31:56 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/94-BinaryTreeInorderTraversal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public List<int> InorderTraversal(TreeNode root)
+    {
+        List<int> result = new List<int>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode current = root;
+
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            current = stack.Pop();
+            result.Add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+}
