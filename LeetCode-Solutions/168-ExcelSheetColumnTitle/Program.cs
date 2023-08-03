51de89f3111dd9e1505220fe6c4dd9277bd6827d
MrAke <aketostoboy@gmail.com>
2023-08-03 08:13:05 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/168-ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        string output = "";
+        while (columnNumber > 0)
+        {
+            char ch = (char)('A' + (columnNumber - 1) % 26);
+            output = ch + output;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        return output;
+    }
+}
