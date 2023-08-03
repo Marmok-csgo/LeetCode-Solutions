712e3b627e9571477158c5c23c8f30f540e68b83
MrAke <aketostoboy@gmail.com>
2023-08-03 08:10:53 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/118-Pascal'sTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        List<IList<int>> t = new List<IList<int>>();
+
+        for (int i = 0; i < numRows; i++)
+        {
+            List<int> r = new List<int>(i + 1);
+            for (int j = 0; j < i + 1; j++)
+            {
+                r.Add(1);
+            }
+
+            for (int j = 1; j < i; j++)
+            {
+                r[j] = t[i - 1][j - 1] + t[i - 1][j];
+            }
+
+            t.Add(r);
+        }
+
+        return t;
+    }
+}
