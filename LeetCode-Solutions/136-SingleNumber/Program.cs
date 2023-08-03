579938fd35967f172ceae42511a7419d8c96873f
MrAke <aketostoboy@gmail.com>
2023-08-03 08:12:15 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/136-SingleNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+
+        foreach (int num in nums)
+        {
+            result ^= num;
+        }
+
+        return result;
+    }
+}
