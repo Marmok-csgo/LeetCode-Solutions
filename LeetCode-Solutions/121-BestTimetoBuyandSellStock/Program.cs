f7767f445224c72534e780249887e251bffe3edb
MrAke <aketostoboy@gmail.com>
2023-08-03 08:11:42 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/121-BestTimetoBuyandSellStock/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices == null || prices.Length == 0)
+        {
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        foreach (int price in prices)
+        {
+            if (price < minPrice)
+            {
+                minPrice = price;
+            }
+            else
+            {
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
