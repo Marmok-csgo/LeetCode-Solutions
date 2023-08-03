00fef83315d8c62a9bb5a8a55192ea75e0e9789d
MrAke <aketostoboy@gmail.com>
2023-08-03 08:12:31 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/141-LinkedListCycle/Program.cs
@@ -0,0 +1,32 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
