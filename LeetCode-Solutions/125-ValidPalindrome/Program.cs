4b5dc8bd679052ddaaa0900a873d3b4b9c3a6e5b
MrAke <aketostoboy@gmail.com>
2023-08-03 08:12:01 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/125-ValidPalindrome/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        string cleanedString = new string(s.ToLower().Where(char.IsLetterOrDigit).ToArray());
+        return cleanedString == new string(cleanedString.Reverse().ToArray());
+    }
+}
