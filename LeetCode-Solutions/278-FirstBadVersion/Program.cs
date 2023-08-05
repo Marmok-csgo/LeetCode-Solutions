26af206ec36a16b4ad6325924f56fb1336446c3e
MrAke <aketostoboy@gmail.com>
2023-08-05 19:49:43 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/278-FirstBadVersion/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        int bad = 0;
+        int low = 1;
+        int high = n;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (IsBadVersion(mid))
+            {
+                bad = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return bad;
+    }
+}
