a7c7497bcfe0c6df3d4e3c3d80295ac338635acf
MrAke <aketostoboy@gmail.com>
2023-08-05 19:50:45 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/268-MissingNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int length = nums.Length + 1;
+        HashSet<int> numSet = new HashSet<int>(nums);
+
+        for (int i = 0; i < length; i++)
+        {
+            if (!numSet.Contains(i))
+            {
+                return i;
+            }
+        }
+
+        throw new InvalidOperationException("Input is invalid.");
+    }
+}
