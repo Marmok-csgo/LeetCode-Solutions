ce3c92f6c5a42cbbe2e0b6aa71a2aecf094e188d
MrAke <aketostoboy@gmail.com>
2023-08-05 19:51:25 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/303-RangeSumQueryImmutable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NumArray
+{
+    private int[] nums;
+
+    public NumArray(int[] nums)
+    {
+        this.nums = new int[nums.Length + 1];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            this.nums[i + 1] = this.nums[i] + nums[i];
+        }
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return this.nums[right + 1] - this.nums[left];
+    }
+}
