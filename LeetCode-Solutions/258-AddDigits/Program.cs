bed82b89ded1c1f5db3e9e0fe866b3c4e70f3d58
MrAke <aketostoboy@gmail.com>
2023-08-05 19:50:25 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/258-AddDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution
+{
+    public int AddDigits(int num)
+    {
+        while (num > 9)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            num = sum;
+        }
+
+        return num;
+    }
+}
