ca7613d83eaf0a47ed7fc296f238953a8da54dd8
MrAke <aketostoboy@gmail.com>
2023-08-05 19:49:58 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/283-MoveZeroes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int zeroPos = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[zeroPos];
+                nums[zeroPos] = temp;
+                zeroPos++;
+            }
+        }
+    }
+}
