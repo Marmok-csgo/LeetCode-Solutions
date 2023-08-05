d3bc4e3ec382c8b0dae8668e02113e7f722f83b5
MrAke <aketostoboy@gmail.com>
2023-08-05 19:50:55 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/263-UglyNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Solution
+{
+    public bool IsUgly(int num)
+    {
+        if (num <= 0)
+        {
+            return false;
+        }
+
+        while (num % 2 == 0)
+        {
+            num /= 2;
+        }
+
+        while (num % 3 == 0)
+        {
+            num /= 3;
+        }
+
+        while (num % 5 == 0)
+        {
+            num /= 5;
+        }
+
+        return num == 1;
+    }
+}
