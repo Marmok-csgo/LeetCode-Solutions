fd0ad957fbe812f08224a4963e91f764d1511ca2
MrAke <aketostoboy@gmail.com>
2023-08-05 19:49:28 +0500

    LeetCode-Solutions

---
+++ LeetCode-Solutions/242-ValidAnagram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        Dictionary<char, int> sFrequency = new Dictionary<char, int>();
+        Dictionary<char, int> tFrequency = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (sFrequency.ContainsKey(c))
+            {
+                sFrequency[c]++;
+            }
+            else
+            {
+                sFrequency[c] = 1;
+            }
+        }
+
+        foreach (char c in t)
+        {
+            if (tFrequency.ContainsKey(c))
+            {
+                tFrequency[c]++;
+            }
+            else
+            {
+                tFrequency[c] = 1;
+            }
+        }
+
+        foreach (char c in sFrequency.Keys)
+        {
+            if (!tFrequency.ContainsKey(c) || tFrequency[c] != sFrequency[c])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
