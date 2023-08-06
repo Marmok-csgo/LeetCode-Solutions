d73fc4b34601e89e48cbd3fb991f387a6bfd58d0
MrAke <aketostoboy@gmail.com>
2023-08-06 18:52:46 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/326-PowerOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        while (n > 0)
+        {
+            if (n == 1)
+                return true;
+            else if (n % 3 == 0)
+                n /= 3;
+            else
+                return false;
+        }
+        return false;
+    }
+}
