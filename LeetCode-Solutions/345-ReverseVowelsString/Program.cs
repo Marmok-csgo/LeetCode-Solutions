3dc762fb025d137ece2697dc74f6ae1fcbe381c1
MrAke <aketostoboy@gmail.com>
2023-08-06 18:53:32 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/345-ReverseVowelsString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public string ReverseVowels(string s)
+    {
+        HashSet<char> vowels = new HashSet<char>("aeiouAEIOU");
+        char[] arr = s.ToCharArray();
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left <= right)
+        {
+            if (vowels.Contains(arr[left]) && vowels.Contains(arr[right]))
+            {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            else if (vowels.Contains(arr[left]))
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return new string(arr);
+    }
+}
