04a48564929f346421c8c337761e3295697c531f
MrAke <aketostoboy@gmail.com>
2023-08-06 18:53:42 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/349-IntersectionTwoArrays/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> result = new HashSet<int>(nums2);
+        result.IntersectWith(nums1);
+        return result.ToArray();
+    }
+}
