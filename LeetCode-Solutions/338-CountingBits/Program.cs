3d16f382bedd00bb0cb209bc497c3f6a1769150a
MrAke <aketostoboy@gmail.com>
2023-08-06 18:53:00 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/338-CountingBits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] CountBits(int n)
+    {
+        int[] list1 = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            int sum = CountOnesInBinary(i);
+            list1[i] = sum;
+        }
+        return list1;
+    }
+
+    private int CountOnesInBinary(int num)
+    {
+        int count = 0;
+        while (num > 0)
+        {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
+}
