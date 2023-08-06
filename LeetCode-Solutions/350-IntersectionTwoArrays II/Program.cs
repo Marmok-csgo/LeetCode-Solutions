9ad48a62e4e68e9e0350dd361ee73d9fab22dfb8
MrAke <aketostoboy@gmail.com>
2023-08-06 18:53:52 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/350-IntersectionTwoArrays II/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        Dictionary<int, int> hashMap = new Dictionary<int, int>();
+        List<int> result = new List<int>();
+
+        foreach (int num in nums1)
+        {
+            if (hashMap.ContainsKey(num))
+                hashMap[num]++;
+            else
+                hashMap[num] = 1;
+        }
+
+        foreach (int num in nums2)
+        {
+            if (hashMap.ContainsKey(num) && hashMap[num] != 0)
+            {
+                result.Add(num);
+                hashMap[num]--;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
