f5ee6152585f49d4f00624d539af55ca88716f19
MrAke <aketostoboy@gmail.com>
2023-08-07 13:27:52 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/392-IsSubsequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int i = 0, j = 0;
+
+        while (i < s.Length && j < t.Length)
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.Length;
+    }
+}
