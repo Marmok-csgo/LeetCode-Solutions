c6f3d6b60d52022ae6c1bbaf870b431e1ddaa332
MrAke <aketostoboy@gmail.com>
2023-08-07 13:28:30 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/412-FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        List<string> resultList = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            string result = (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                            (i % 3 == 0) ? "Fizz" :
+                            (i % 5 == 0) ? "Buzz" : i.ToString();
+
+            resultList.Add(result);
+        }
+
+        return resultList;
+    }
+}
