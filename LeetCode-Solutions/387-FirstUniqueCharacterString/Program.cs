829ec507adc0642181002f89172cddeafc65d766
MrAke <aketostoboy@gmail.com>
2023-08-07 13:27:29 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/387-FirstUniqueCharacterString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        Dictionary<char, int> contador = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (contador.ContainsKey(c))
+                contador[c]++;
+            else
+                contador[c] = 1;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (contador[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
