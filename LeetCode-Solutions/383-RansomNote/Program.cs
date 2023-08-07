2725fc459962b5544e03ad7d2685e1b753ecfac0
MrAke <aketostoboy@gmail.com>
2023-08-07 13:27:18 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/383-RansomNote/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var ransomNoteCounter = new Dictionary<char, int>();
+        var magazineCounter = new Dictionary<char, int>();
+
+        foreach (char c in ransomNote)
+        {
+            if (ransomNoteCounter.ContainsKey(c))
+                ransomNoteCounter[c]++;
+            else
+                ransomNoteCounter[c] = 1;
+        }
+
+        foreach (char c in magazine)
+        {
+            if (magazineCounter.ContainsKey(c))
+                magazineCounter[c]++;
+            else
+                magazineCounter[c] = 1;
+        }
+
+        foreach (var kvp in ransomNoteCounter)
+        {
+            if (!magazineCounter.ContainsKey(kvp.Key) || magazineCounter[kvp.Key] < kvp.Value)
+                return false;
+        }
+
+        return true;
+    }
+}
