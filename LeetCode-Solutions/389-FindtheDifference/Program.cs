ef7cdb8d81296f880b299cf059175fcad996f8cd
MrAke <aketostoboy@gmail.com>
2023-08-07 13:27:42 +0500

    LeetCode-Solution

---
+++ LeetCode-Solutions/389-FindtheDifference/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        if (s.Length > t.Length)
+        {
+            foreach (char c in s)
+            {
+                if (CountOccurrences(s, c) > CountOccurrences(t, c))
+                {
+                    return c;
+                }
+            }
+        }
+
+        if (t.Length > s.Length)
+        {
+            foreach (char c in t)
+            {
+                if (CountOccurrences(t, c) > CountOccurrences(s, c))
+                {
+                    return c;
+                }
+            }
+        }
+
+        return '\0';
+    }
+
+    private int CountOccurrences(string str, char c)
+    {
+        int count = 0;
+        foreach (char ch in str)
+        {
+            if (ch == c)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
