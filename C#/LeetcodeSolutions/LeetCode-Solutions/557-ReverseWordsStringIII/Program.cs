4fd2b6e5577b7b3dca47f3a58fda81f1f3d6c04c
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-11 11:33:54 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/557-ReverseWordsStringIII/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[] words = s.Split();
+        string[] reversedWords = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] wordChars = words[i].ToCharArray();
+            Array.Reverse(wordChars);
+            reversedWords[i] = new string(wordChars);
+        }
+
+        return string.Join(" ", reversedWords);
+    }
+}
