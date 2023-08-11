f1cd8655e7aed2d4013115ca61e0e7a805088608
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-11 11:21:47 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/509-FibonacciNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int Fib(int n)
+    {
+        Dictionary<int, int> mydict = new Dictionary<int, int>();
+        return FibNum(n, mydict);
+    }
+
+    private int FibNum(int x, Dictionary<int, int> dic1)
+    {
+        if (x == 0 || x == 1)
+        {
+            return x;
+        }
+        else if (!dic1.ContainsKey(x))
+        {
+            dic1[x] = FibNum(x - 1, dic1) + FibNum(x - 2, dic1);
+        }
+        return dic1[x];
+    }
+}
