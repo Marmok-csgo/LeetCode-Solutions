a151b6e8d68cc6da709a333f0d249046908cbcd5
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-11 11:33:40 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/551-StudentAttendanceRecordI/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public bool CheckRecord(string s)
+    {
+        if (s.Count(c => c == 'A') >= 2 || s.Contains("LLL"))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
