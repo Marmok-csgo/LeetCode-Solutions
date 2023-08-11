84647f356ec6382bef5521f826a63f18d6b74e9e
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-11 11:34:21 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/566-ReshapetheMatrix/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        List<int> list1 = mat.SelectMany(row => row).ToList();
+        List<int[]> matrix = new List<int[]>();
+
+        if (mat.Length * mat[0].Length != r * c)
+        {
+            return mat;
+        }
+
+        for (int i = 0; i < list1.Count; i += c)
+        {
+            matrix.Add(list1.GetRange(i, Math.Min(c, list1.Count - i)).ToArray());
+        }
+
+        return matrix.ToArray();
+    }
+}
