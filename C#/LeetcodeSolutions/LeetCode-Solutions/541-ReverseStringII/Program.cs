f7a122b0cd4381dcb13b446e9b61384e535970d6
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-11 11:33:22 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/541-ReverseStringII/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public string ReverseStr(string s, int k)
+    {
+        if (s.Length < k)
+            return new string(s.Reverse().ToArray());
+
+        if (s.Length < 2 * k)
+            return new string(s.Substring(0, k).Reverse().ToArray()) + s.Substring(k);
+
+        return new string(s.Substring(0, k).Reverse().ToArray()) +
+               s.Substring(k, k) +
+               ReverseStr(s.Substring(2 * k), k);
+    }
+}
