a77396e766a64a421ae1bb561511f5310194d301
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-11 11:34:08 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/561-ArrayPartition/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int ArrayPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+        int maxSum = 0;
+        
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            maxSum += nums[i];
+        }
+        
+        return maxSum;
+    }
+}
