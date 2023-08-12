f2fa751526d646df70c38be374e8f35a4b5b908f
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-12 12:24:50 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/628-MaximumProductThreeNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public class Solution {
+    public int MaximumProduct(int[] nums) {
+        Array.Sort(nums);
+        int n = nums.Length;
+
+        return Math.Max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
+    }
+}
