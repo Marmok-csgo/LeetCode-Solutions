b94e242d28ba147705344bc9e1f3e8df834277f8
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-12 12:25:16 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/645-SetMismatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] FindErrorNums(int[] nums) {
+        Dictionary<int, int> counter = new Dictionary<int, int>();
+        int[] result = new int[2];
+        
+        foreach (int num in nums) {
+            if (!counter.ContainsKey(num)) {
+                counter[num] = 1;
+            }
+            else {
+                counter[num]++;
+            }
+        }
+        
+        for (int i = 1; i <= nums.Length; i++) {
+            if (counter.ContainsKey(i)) {
+                if (counter[i] == 2) {
+                    result[0] = i;
+                }
+            }
+            else {
+                result[1] = i;
+            }
+        }
+        
+        return result;
+    }
+}
