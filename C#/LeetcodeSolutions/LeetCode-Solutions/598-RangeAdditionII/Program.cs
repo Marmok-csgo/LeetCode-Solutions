ba49b9711bb8ca6008cfdfdf02369e2d16d67696
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-12 12:23:54 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/598-RangeAdditionII/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int MaxCount(int m, int n, int[][] ops) {
+        int min_y = m;
+        int min_x = n;
+
+        foreach (var op in ops) {
+            int y = op[0];
+            int x = op[1];
+            
+            min_y = Math.Min(min_y, y);
+            min_x = Math.Min(min_x, x);
+        }
+
+        return min_x * min_y;
+    }
+}
