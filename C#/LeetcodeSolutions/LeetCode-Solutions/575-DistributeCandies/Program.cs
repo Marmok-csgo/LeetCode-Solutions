72d95c4b429a48845dde508ad560567ae584f78a
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-12 12:23:36 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/575-DistributeCandies/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int DistributeCandies(int[] candyType) {
+        HashSet<int> setCandyType = new HashSet<int>(candyType);
+        int count = candyType.Length / 2;
+        
+        if (count <= setCandyType.Count) {
+            return count;
+        }
+        else {
+            return setCandyType.Count;
+        }
+    }
+}
