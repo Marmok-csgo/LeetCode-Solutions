c0fa649a5826f338acd5b9b64790f9a57d915616
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-12 12:24:26 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/605-CanPlaceFlowers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int i = 0;
+        
+        while (i < flowerbed.Length) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            i++;
+        }
+
+        return count >= n;
+    }
+}
