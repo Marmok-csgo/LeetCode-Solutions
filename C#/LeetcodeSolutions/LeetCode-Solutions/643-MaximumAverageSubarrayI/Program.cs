00a0d7f01fea91e4997945c9579461cc82d2d6cc
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-12 12:25:03 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/643-MaximumAverageSubarrayI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution {
+    public double FindMaxAverage(int[] nums, int k) {
+        int s = 0;
+        for (int i = 0; i < k; i++) {
+            s += nums[i];
+        }
+        
+        int maxSum = s;
+        
+        for (int i = 0; i < nums.Length - k; i++) {
+            s = s + nums[i + k] - nums[i];
+            maxSum = Math.Max(maxSum, s);
+        }
+
+        return (double)maxSum / k;
+    }
+}
