e9ad25c41c751934ba9267b97973c72990cbd71f
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-13 17:24:33 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/728-SelfDividingNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public IList<int> SelfDividingNumbers(int left, int right)
+    {
+        List<int> v = new List<int>();
+
+        for (int x = left; x <= right; x++)
+        {
+            List<char> a = new List<char>(x.ToString().ToCharArray());
+            bool d = true;
+
+            foreach (char r in a)
+            {
+                if (r != '0')
+                {
+                    if (x % (r - '0') != 0)
+                    {
+                        d = false;
+                    }
+                }
+                else
+                {
+                    d = false;
+                }
+            }
+
+            if (d)
+            {
+                v.Add(x);
+            }
+        }
+
+        return v;
+    }
+}
