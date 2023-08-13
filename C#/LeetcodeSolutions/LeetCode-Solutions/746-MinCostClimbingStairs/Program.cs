8825c3a230286336663270de1ce103b9414ad56d
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-13 17:25:04 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/746-MinCostClimbingStairs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int prev = 0;
+        int prev2 = 0;
+        int curr = 0;
+
+        for (int i = 2; i <= cost.Length; i++)
+        {
+            curr = Math.Min(prev + cost[i - 1], prev2 + cost[i - 2]);
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
+}
