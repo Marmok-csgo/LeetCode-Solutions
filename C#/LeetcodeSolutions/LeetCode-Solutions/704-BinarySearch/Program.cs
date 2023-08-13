db03d113c2935481bf872acdda3f17a3d5acadce
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-13 17:23:23 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/704-BinarySearch/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
