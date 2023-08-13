d03f46b067e04480fa50464b232f8be9521d474a
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-13 17:24:51 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/744-FindSmallestLetterGreaterThanTarget/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        int l = 0;
+        int r = letters.Length;
+
+        while (l < r)
+        {
+            int mid = (l + r) / 2;
+            if (letters[mid] > target)
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return r < letters.Length ? letters[r] : letters[0];
+    }
+}
