1977832841c15ed19fb00ff08a2d18ff5d6124a0
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-13 17:24:09 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/724-FindPivotIndex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int totalSum = 0;
+        foreach (int num in nums)
+        {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum == totalSum - leftSum - nums[i])
+            {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
