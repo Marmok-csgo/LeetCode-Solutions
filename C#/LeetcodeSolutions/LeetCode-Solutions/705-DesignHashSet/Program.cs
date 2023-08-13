d0880b401f11b3b5fc9048c0f2cb1480839df490
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-13 17:23:39 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/705-DesignHashSet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyHashSet
+{
+    private HashSet<int> keys;
+
+    public MyHashSet()
+    {
+        keys = new HashSet<int>();
+    }
+
+    public void Add(int key)
+    {
+        keys.Add(key);
+    }
+
+    public void Remove(int key)
+    {
+        keys.Remove(key);
+    }
+
+    public bool Contains(int key)
+    {
+        return keys.Contains(key);
+    }
+}
