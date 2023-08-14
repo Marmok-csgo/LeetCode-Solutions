a4cd8b42b220dcd6a32317a414b1a3f9f61d61e1
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-14 14:03:40 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/762-PrimeNumberSetBitsBinaryRepresentation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    private static readonly HashSet<int> Primes = new HashSet<int> { 2, 3, 5, 7, 11, 13, 17, 19 };
+
+    public int CountPrimeSetBits(int left, int right)
+    {
+        int count = 0;
+        
+        for (int i = left; i <= right; i++)
+        {
+            if (IsPrimeSetBits(i))
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+
+    private bool IsPrimeSetBits(int num)
+    {
+        int setBitsCount = CountSetBits(num);
+        return Primes.Contains(setBitsCount);
+    }
+
+    private int CountSetBits(int num)
+    {
+        int count = 0;
+        
+        while (num > 0)
+        {
+            count += num & 1;
+            num >>= 1;
+        }
+        
+        return count;
+    }
+}
