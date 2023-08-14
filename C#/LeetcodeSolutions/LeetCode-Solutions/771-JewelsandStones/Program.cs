c40eae6ad345d63794141fcad298965fa3766c9b
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-14 14:03:54 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/771-JewelsandStones/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int NumJewelsInStones(string jewels, string stones) {
+        HashSet<char> jewelSet = new HashSet<char>(jewels);
+
+        int count = 0;
+        foreach (char stone in stones) {
+            if (jewelSet.Contains(stone)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
