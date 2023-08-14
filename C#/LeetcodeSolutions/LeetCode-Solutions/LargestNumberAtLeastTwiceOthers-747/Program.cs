51df7fa2f0397486487a5a7fa1cfaacb7b06e0c0
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-14 14:05:56 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/LargestNumberAtLeastTwiceOthers-747/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int DominantIndex(int[] nums)
+    {
+        List<int> l = nums.OrderByDescending(n => n).ToList();
+        
+        if (l[0] >= 2 * l[1])
+        {
+            return Array.IndexOf(nums, nums.Max());
+        }
+        
+        return -1;
+    }
+}
