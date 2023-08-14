5fbf97a672dc1f90d868fd2e81a6c9c6ec77ee87
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-14 14:02:14 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/748-ShortestCompletingWord/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution {
+    public string ShortestCompletingWord(string licensePlate, string[] words) {
+        string res = "";
+        bool find = false;
+        var dict = new Dictionary<char, int>();
+        for (int i = 0; i < licensePlate.Length; i++)
+        {
+            var ch = licensePlate[i];
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                if (dict.ContainsKey(char.ToLower(ch)))
+                    dict[char.ToLower(ch)]++;
+                else dict[char.ToLower(ch)] = 1;
+            }
+        }
+        for (int i = 0; i < words.Length; i++)
+        {
+            foreach(var item in dict)
+            {
+                if (words[i].Count(x => x == item.Key) < item.Value) 
+                { find = false; break;}
+                find = true;
+            }
+            if (find)
+            {
+                if (res == "") res = words[i];
+                else if (words[i].Length < res.Length)
+                    res = words[i];
+            }
+        }
+        return res;
+    }
+}
