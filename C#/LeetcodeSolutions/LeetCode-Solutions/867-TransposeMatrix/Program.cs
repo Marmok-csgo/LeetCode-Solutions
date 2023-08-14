811f4d383888636d641534cd3171a9ddc38a3b4b
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-14 14:05:43 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/867-TransposeMatrix/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int[][] Transpose(int[][] A) {
+        int[][] res = new int[A[0].Length][];
+        for (int i = 0; i < res.Length; i++)
+            res[i] = new int[A.Length];
+        for (int i = 0; i < A.Length; i++)
+        {
+            for (int j = 0; j < A[0].Length; j++)
+                res[j][i] = A[i][j];
+        }
+        return res;
+    }
+}
