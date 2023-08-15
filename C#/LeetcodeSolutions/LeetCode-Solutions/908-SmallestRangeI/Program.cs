86d0307f2c82b50166fa2b3c10f91c3528e8523d
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-15 08:49:22 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/908-SmallestRangeI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int SmallestRangeI(int[] A, int K)
+    {
+        int max = A.Max();
+        int min = A.Min();
+        int diff = max - min;
+        int ex = 2 * K;
+
+        if (diff <= ex)
+        {
+            return 0;
+        }
+        else
+        {
+            return diff - ex;
+        }
+    }
+}
