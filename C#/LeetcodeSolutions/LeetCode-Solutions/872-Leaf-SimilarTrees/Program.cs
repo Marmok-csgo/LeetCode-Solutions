03201a44945eef6de6d68ee1938ee4e9d817ad31
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-15 08:48:32 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/872-Leaf-SimilarTrees/Program.cs
@@ -0,0 +1,57 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    private List<int> retList = new List<int>();
+    private List<int> retList1 = new List<int>();
+
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        Traversal(root1);
+        Traversal2(root2);
+        return retList.SequenceEqual(retList1);
+    }
+
+    private bool IsLeaf(TreeNode root)
+    {
+        return root.left == null && root.right == null;
+    }
+
+    private void Traversal(TreeNode root)
+    {
+        if (root != null)
+        {
+            Traversal(root.left);
+            if (IsLeaf(root))
+            {
+                retList.Add(root.val);
+            }
+            Traversal(root.right);
+        }
+    }
+
+    private void Traversal2(TreeNode root)
+    {
+        if (root != null)
+        {
+            Traversal2(root.left);
+            if (IsLeaf(root))
+            {
+                retList1.Add(root.val);
+            }
+            Traversal2(root.right);
+        }
+    }
+}
