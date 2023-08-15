ba48c13c8b9d04bada9534e0b448ba35057c8a2c
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-15 08:49:13 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/905-SortArrayByParity/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        List<int> evenNumbers = nums.Where(i => i % 2 == 0).ToList();
+        List<int> oddNumbers = nums.Where(j => j % 2 != 0).ToList();
+        
+        evenNumbers.AddRange(oddNumbers);
+        
+        return evenNumbers.ToArray();
+    }
+}
