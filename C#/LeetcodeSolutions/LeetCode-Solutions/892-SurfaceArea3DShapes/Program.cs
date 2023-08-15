5fe0a35356a0b17e89aff5d7816fe6018e555c8d
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-15 08:49:05 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/892-SurfaceArea3DShapes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int SurfaceArea(int[][] grid)
+    {
+        int area = 0;
+        int n = grid.Length;
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] != 0)
+                {
+                    area += grid[i][j] * 4 + 2;
+                }
+                
+                if (i > 0)
+                {
+                    area -= Math.Min(grid[i][j], grid[i - 1][j]) * 2;
+                }
+                
+                if (j > 0)
+                {
+                    area -= Math.Min(grid[i][j], grid[i][j - 1]) * 2;
+                }
+            }
+        }
+        
+        return area;
+    }
+}
