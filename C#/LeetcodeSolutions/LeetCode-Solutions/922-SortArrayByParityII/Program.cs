799c22864405441a788c3be417e3c6ed66aeba16
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-15 08:49:49 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/922-SortArrayByParityII/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int[] SortArrayByParityII(int[] nums) {
+        List<int> answer = new List<int>();
+
+        List<int> even = new List<int>();
+        List<int> odd = new List<int>();
+
+        foreach (int n in nums) {
+            if (n % 2 == 0) {
+                even.Add(n);
+            } else {
+                odd.Add(n);
+            }
+        }
+
+        for (int i = 0; i < even.Count; i++) {
+            answer.Add(even[i]);
+            answer.Add(odd[i]);
+        }
+
+        return answer.ToArray();
+    }
+}
