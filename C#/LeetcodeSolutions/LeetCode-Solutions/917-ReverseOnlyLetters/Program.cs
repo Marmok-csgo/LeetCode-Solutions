982112989dd229934d8658556929dd32712a5440
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-15 08:49:41 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/917-ReverseOnlyLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution {
+    public string ReverseOnlyLetters(string s) {
+        char[] charArray = s.ToCharArray();
+        int left = 0;
+        int right = charArray.Length - 1;
+        
+        while (left < right) {
+            if (!Char.IsLetter(charArray[left])) {
+                left++;
+            }
+            else if (!Char.IsLetter(charArray[right])) {
+                right--;
+            }
+            else {
+                char temp = charArray[left];
+                charArray[left] = charArray[right];
+                charArray[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        return new string(charArray);
+    }
+}
