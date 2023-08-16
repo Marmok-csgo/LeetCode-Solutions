fd8395e1781119cba10d7a1a285c3025b1d5fef3
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-16 12:54:10 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/989-AddtoArray-FormofInteger/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Numerics;
+public class Solution {
+    public IList<int> AddToArrayForm(int[] num, int k) {
+        return (BigInteger.Parse(string.Join("", num.Select(x => x))) + k).ToString().Select(x => int.Parse(x.ToString())).ToList();
+    }
+}
