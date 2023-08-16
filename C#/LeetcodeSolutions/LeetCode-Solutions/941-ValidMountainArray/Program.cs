765af5276083971ba82e00e46e865bdb3acc07b8
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-16 12:53:32 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/941-ValidMountainArray/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public bool ValidMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right) {
+            if (arr[left] < arr[left + 1]) {
+                left++;
+            } else if (arr[right - 1] > arr[right]) {
+                right--;
+            } else {
+                break;
+            }
+        }
+
+        return arr.Length >= 3 && 0 < left && left == right && right < arr.Length - 1;
+    }
+}
