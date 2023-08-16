b5457f381f27f6c8916442c8a9971053bc82b80e
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-16 12:53:09 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/933-NumberRecentCalls/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RecentCounter
+{
+    private Queue<int> queue;
+
+    public RecentCounter()
+    {
+        queue = new Queue<int>();
+    }
+
+    public int Ping(int t)
+    {
+        queue.Enqueue(t);
+        while (queue.Peek() < t - 3000)
+        {
+            queue.Dequeue();
+        }
+        return queue.Count;
+    }
+}
