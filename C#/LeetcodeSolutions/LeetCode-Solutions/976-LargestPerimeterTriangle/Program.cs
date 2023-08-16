9fa37493562399b133aa23b56ff6c95f70d57325
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-16 12:53:49 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/976-LargestPerimeterTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        Array.Reverse(nums);
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            int baseValue = nums[i];
+            int side1 = nums[i + 1];
+            int side2 = nums[i + 2];
+
+            if (side1 + side2 > baseValue)
+            {
+                return baseValue + side1 + side2;
+            }
+        }
+
+        return 0;
+    }
+}
