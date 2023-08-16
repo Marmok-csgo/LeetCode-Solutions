85eb944816f774d2b21d1de3a4aee5db59b22d2a
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-16 12:54:01 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/977-SquaresSortedArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        List<int> resultList = new List<int>();
+
+        foreach (int num in nums)
+        {
+            int squared = (int)Math.Pow(num, 2);
+            resultList.Add(squared);
+        }
+
+        return resultList.OrderBy(x => x).ToArray();
+    }
+}
