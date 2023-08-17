2149d402b73154e7abcb6bcfbddf081c1d01912f
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-17 15:05:46 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1047-RemoveAllAdjacentDuplicatesString/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public string RemoveDuplicates(string s) {
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i].ToString() == s[i + 1].ToString())
+            {
+                s = s.Remove(i, 2); 
+                i = Math.Max(-1, i - 2);
+            }
+        }
+        return s;
+    }
+}
