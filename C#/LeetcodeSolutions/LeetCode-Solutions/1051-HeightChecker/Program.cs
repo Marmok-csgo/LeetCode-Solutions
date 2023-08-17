810b9ed893a4312a7a957f80a958e3cbdaeb0f87
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-17 15:06:15 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1051-HeightChecker/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int HeightChecker(int[] heights) {
+        int[] sortedHeights = (int[])heights.Clone();
+        Array.Sort(sortedHeights);
+        
+        int count = 0;
+        for (int i = 0; i < heights.Length; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
