6390a886295e2ddfc30276b20719b945dbce9b83
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-17 15:06:41 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1071-GreatestCommonDivisorofStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        if (str1.Length < str2.Length)
+        {
+            string temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
+
+        if (str1 == str2)
+        {
+            return str1;
+        }
+
+        if (str1.StartsWith(str2))
+        {
+            return GcdOfStrings(str1.Substring(str2.Length), str2);
+        }
+
+        return "";
+    }
+}
