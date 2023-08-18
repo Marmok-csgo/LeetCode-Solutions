3c0fc0f3a071d4b1aa21464de7206bf18cc8b2eb
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-18 14:47:07 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1200-MinimumAbsoluteDifference/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public IList<IList<int>> MinimumAbsDifference(int[] arr) {
+        
+        Array.Sort(arr);
+        
+        int minimum_differece = Int32.MaxValue;
+        
+        List<IList<int>> ans = new List<IList<int>>();
+        
+        for(int i = 0; i < arr.Length-1; i++) {
+            minimum_differece = Math.Min(minimum_differece, arr[i + 1] - arr[i]);
+        }
+        
+        for(int i = 0; i < arr.Length-1; i++) {
+            if(arr[i + 1] - arr[i] == minimum_differece)
+                ans.Add(new List<int>{arr[i], arr[i+1]});
+        }
+        
+        return ans;
+    }
+}
