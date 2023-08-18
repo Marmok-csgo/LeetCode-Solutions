4a3227827a3c72847b22acc8e5ae63d8bed7b220
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-18 14:45:46 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1089-DuplicateZeros/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public void DuplicateZeros(int[] arr) {
+        int countOfZeros = arr.Count(num => num == 0);
+        int lastIndex = arr.Length + countOfZeros - 1;
+
+        for (int i = arr.Length - 1; i >= 0; i--) {
+            if (lastIndex < arr.Length) {
+                arr[lastIndex] = arr[i];
+            }
+            lastIndex--;
+
+            if (arr[i] == 0) {
+                if (lastIndex < arr.Length) {
+                    arr[lastIndex] = 0;
+                }
+                lastIndex--;
+            }
+        }
+    }
+}
