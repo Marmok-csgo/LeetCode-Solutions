46603089f09b309663efe0636c1fa9e4507e577e
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-18 14:46:00 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1103-DistributeCandiestoPeople/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int[] DistributeCandies(int candies, int n) {
+        int[] res = new int[n];
+        int i = 0;
+        
+        while (candies > 0) {
+            res[i % n] += Math.Min(candies, i + 1);
+            candies -= i + 1;
+            i++;
+        }
+        
+        return res;
+    }
+}
