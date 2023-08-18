4a8ae1826509a5ff281e703888e0387d76245bba
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-18 14:46:57 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1185-DayoftheWeek/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Solution
+{
+    public string DayOfTheWeek(int d, int m, int y)
+    {
+        DateTime date = new DateTime(y, m, d);
+        return date.ToString("dddd");
+    }
+}
