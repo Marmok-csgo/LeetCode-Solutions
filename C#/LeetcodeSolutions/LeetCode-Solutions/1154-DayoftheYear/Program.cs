eea26396b5383f890bd304c699aa555dfe75bdff
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-18 14:46:38 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1154-DayoftheYear/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int DayOfYear(string date) {
+        string[] dateParts = date.Split('-');
+        int y = int.Parse(dateParts[0]);
+        int m = int.Parse(dateParts[1]);
+        int d = int.Parse(dateParts[2]);
+        
+        if ((y % 400 == 0 || (y % 100 != 0 && y % 4 == 0)) && m > 2) {
+            d++;
+        }
+        
+        int[] months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        
+        for (int i = 1; i < m; i++) {
+            d += months[i - 1];
+        }
+        
+        return d;
+    }
+}
