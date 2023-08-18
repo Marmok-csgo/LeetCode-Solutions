a70f57197797f89b815c82a251512ba84e5ded08
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-18 14:46:10 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1137-N-thTribonacciNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int Tribonacci(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int d = 0;
+        for (int i = 3; i <= n; i++) {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return d;
+    }
+}
