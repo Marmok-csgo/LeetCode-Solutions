ac53eaa5f599f69ec59708b5cec7676df3d0aa58
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-19 14:03:56 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1287-ElementAppearingMoreThan25InSortedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int FindSpecialInteger(int[] arr)
+    {
+        int check = arr.Length / 4;
+        
+        foreach (int num in new HashSet<int>(arr))
+        {
+            int count = 0;
+            foreach (int n in arr)
+            {
+                if (n == num)
+                    count++;
+                if (count > check)
+                    return num;
+            }
+        }
+
+        throw new InvalidOperationException("No special integer found.");
+    }
+}
