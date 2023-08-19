cb58da939a8f9849f7da0fcc1c79fce618ef8da9
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-19 14:03:42 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1281-SubtracttheProductandSumofDigitsanInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int SubtractProductAndSum(int n)
+    {
+        List<int> l = new List<int>();
+        int a = 0;
+        int b = 1;
+        
+        while (n != 0)
+        {
+            int rem = n % 10;
+            l.Add(rem);
+            n = n / 10;
+        }
+        
+        foreach (int i in l)
+        {
+            a += i;
+            b *= i;
+        }
+
+        return b - a;
+    }
+}
