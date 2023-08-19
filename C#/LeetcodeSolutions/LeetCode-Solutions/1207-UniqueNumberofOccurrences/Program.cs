3827a291d14eaa84a0a0ea4054e346075d194373
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-19 14:02:32 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1207-UniqueNumberofOccurrences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> m = new Dictionary<int, int>();
+        foreach (int num in arr)
+        {
+            if (m.ContainsKey(num))
+                m[num]++;
+            else
+                m[num] = 1;
+        }
+
+        HashSet<int> s = new HashSet<int>();
+        foreach (int cnt in m.Values)
+        {
+            s.Add(cnt);
+        }
+
+        return s.Count == m.Count;
+    }
+}
