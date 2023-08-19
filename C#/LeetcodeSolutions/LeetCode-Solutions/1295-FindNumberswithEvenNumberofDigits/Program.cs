d9ed85da4488d0cca1087cc822bafb3b7ffafb07
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-19 14:04:08 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1295-FindNumberswithEvenNumberofDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int FindNumbers(int[] nums)
+    {
+        int count = 0;
+        
+        foreach (int num in nums)
+        {
+            if (num.ToString().Length % 2 == 0)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
