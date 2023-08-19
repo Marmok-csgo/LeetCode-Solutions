ad41f5dc4beb099832e31098cd56ebe81f4395a6
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-19 14:04:23 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1299-ReplaceElementswithGreatestElementonRightSide/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        int maxi = -1;
+        int n = arr.Length;
+        
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int temp = arr[i];
+            arr[i] = maxi;
+            maxi = Math.Max(maxi, temp);
+        }
+        
+        return arr;
+    }
+}
