60a3e789bc41150689b8b2f2e9d9dad1facd1e85
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-19 14:03:29 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1260-Shift2DGrid/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution {
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k) {
+        IList<IList<int>> retVal = new List<IList<int>>();
+        
+        k = k % (grid.Length * grid[0].Length);
+        
+        for (int i = 0; i < grid.Length; ++i)
+        {
+            List<int> newRow = new List<int>();
+            retVal.Insert(i, newRow);
+            for (int j = 0; j < grid[0].Length; ++j)            
+                newRow.Insert(j, 0);            
+        }
+                    
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; ++j)
+            {   
+                int distFromZero = i * grid[0].Length + j; 
+                int shiftTo = (distFromZero + k) % (grid.Length * grid[0].Length); 
+                int newRow = shiftTo / grid[0].Length;
+                int newCol = shiftTo % grid[0].Length;
+                retVal[newRow][newCol] = grid[i][j];                                
+            }
+        }
+        return retVal;
+    }
+}
