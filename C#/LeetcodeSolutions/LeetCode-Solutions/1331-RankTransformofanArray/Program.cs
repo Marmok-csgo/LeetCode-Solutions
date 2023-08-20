0cb6dda72bbd289d900bef8eeba3ceddbcd645c2
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:34:57 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1331-RankTransformofanArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] ArrayRankTransform(int[] arr)
+    {
+        Dictionary<int, int> rank = new Dictionary<int, int>();
+        foreach (int a in arr.OrderBy(x => x))
+        {
+            if (!rank.ContainsKey(a))
+            {
+                rank[a] = rank.Count + 1;
+            }
+        }
+
+        return arr.Select(a => rank[a]).ToArray();
+    }
+}
