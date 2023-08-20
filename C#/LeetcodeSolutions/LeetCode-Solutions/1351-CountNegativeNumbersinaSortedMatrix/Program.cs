e00a9911660a55857665de8ffe01d63afbfd86ef
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:35:31 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1351-CountNegativeNumbersinaSortedMatrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int CountNegatives(int[][] grid)
+    {
+        int count = 0;
+
+        foreach (int[] row in grid)
+        {
+            foreach (int num in row)
+            {
+                if (num < 0)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
