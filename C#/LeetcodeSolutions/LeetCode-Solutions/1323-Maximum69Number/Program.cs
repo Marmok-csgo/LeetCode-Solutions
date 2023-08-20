c607e92f6bbcae6c8518768f5f9efbfad21cafc5
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:34:43 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1323-Maximum69Number/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int Maximum69Number(int num)
+    {
+        int m = num;
+        string s = num.ToString();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int temp;
+            if (s[i] == '6')
+            {
+                temp = int.Parse(s.Substring(0, i) + '9' + s.Substring(i + 1));
+            }
+            else
+            {
+                temp = int.Parse(s.Substring(0, i) + '6' + s.Substring(i + 1));
+            }
+            m = Math.Max(m, temp);
+        }
+
+        return m;
+    }
+}
