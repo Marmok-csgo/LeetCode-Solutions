769595e84240bfdbc589f010b00aa8c346321efb
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:35:11 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1342-NumberofStepstoReduceaNumbertoZero/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+        int count = 0;
+
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+                count++;
+            }
+            else
+            {
+                num -= 1;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
