d10845b0b16d83f7a8474a81e6cfbcd85b450dcd
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:34:18 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1304-FindNUniqueIntegersSumuptoZero/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] SumZero(int n)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            result.Add(i * 2 - n + 1);
+        }
+
+        return result.ToArray();
+    }
+}
