a1fc6a7e5e0553916e38de65985a8f40ba56d7fc
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:35:22 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1346-CheckIfNandItsDoubleExist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        if (arr.Count(x => x == 0) > 1)
+        {
+            return true;
+        }
+        
+        HashSet<int> S = new HashSet<int>(arr.Where(x => x != 0));
+        
+        foreach (int i in arr)
+        {
+            if (S.Contains(2 * i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
