6ab94570b792a4a796c54c5c90ccc133781752a8
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:34:32 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1317-ConvertIntegertheSumofTwoNo-ZeroIntegers/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool NZ(int num)
+    {
+        return !num.ToString().Contains('0');
+    }
+
+    public int[] GetNoZeroIntegers(int n)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            if (NZ(i) && NZ(n - i))
+            {
+                return new int[] { i, n - i };
+            }
+        }
+
+        return null; 
+    }
+}
