662e67e989da64e37dd5b94bfe3e5981e986d6d7
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-20 10:35:41 +0500

    LeetCode-Solutions

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1360-NumberofDaysBetweenTwoDates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public int DaysBetweenDates(string date1, string date2)
+    {
+        DateTime D1 = new DateTime(
+            int.Parse(date1.Substring(0, 4)),
+            int.Parse(date1.Substring(5, 2)),
+            int.Parse(date1.Substring(8))
+        );
+
+        DateTime D2 = new DateTime(
+            int.Parse(date2.Substring(0, 4)),
+            int.Parse(date2.Substring(5, 2)),
+            int.Parse(date2.Substring(8))
+        );
+
+        TimeSpan Res = D1 - D2;
+        return Math.Abs(Res.Days);
+    }
+}
