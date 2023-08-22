d394c5dd829c07eba3eba8dd055d59892bbcf54c
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-22 11:42:45 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1431-KidsWiththeGreatestNumberofCandies/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        int maxCandies = candies.Max();
+
+        List<bool> result = new List<bool>();
+        foreach (int c in candies)
+        {
+            result.Add(c + extraCandies >= maxCandies);
+        }
+
+        return result;
+    }
+}
