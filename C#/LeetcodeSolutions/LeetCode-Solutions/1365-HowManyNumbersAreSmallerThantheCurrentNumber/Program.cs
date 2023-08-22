50a1f4407a107ecefc7b24aff69ea504b63aebce
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-22 11:41:44 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1365-HowManyNumbersAreSmallerThantheCurrentNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        List<int> lst = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    count++;
+                }
+            }
+            lst.Add(count);
+        }
+
+        return lst.ToArray();
+    }
+}
