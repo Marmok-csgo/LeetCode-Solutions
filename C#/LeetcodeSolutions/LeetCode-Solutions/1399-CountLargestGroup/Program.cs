ac045591c94347435cdadb2628f3054b2ef07acf
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-22 11:42:16 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1399-CountLargestGroup/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int CountLargestGroup(int n)
+    {
+        List<int> res = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            int sumDigits = i.ToString().Sum(c => int.Parse(c.ToString()));
+            res.Add(sumDigits);
+        }
+        
+        Dictionary<int, int> counter = res.GroupBy(x => x)
+            .ToDictionary(g => g.Key, g => g.Count());
+        
+        int maxValue = counter.Values.Max();
+        int count = counter.Count(kv => kv.Value == maxValue);
+        
+        return count;
+    }
+}
