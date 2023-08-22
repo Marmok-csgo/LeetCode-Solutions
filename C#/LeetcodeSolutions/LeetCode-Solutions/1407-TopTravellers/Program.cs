65565daf1accca2760ea183f71303d8f50719b24
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-22 11:42:28 +0500

    LeetCode-Solution

---
+++ C#/LeetcodeSolutions/LeetCode-Solutions/1407-TopTravellers/Program.cs
@@ -0,0 +1,5 @@
+﻿// select users.name, ifnull(sum(rides.distance),0) as travelled_distance
+// from users left join rides
+// on users.id = rides.user_id
+// group by user_id
+// order by travelled_distance desc, name asc
